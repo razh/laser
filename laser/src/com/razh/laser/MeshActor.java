db4c89c8153dd6caa8e669beca5531b9d499b408
Raymond Zhou <somecrazyidiot@gmail.com>
2013-03-03 11:30:29 -0500

    Work on Shader and MeshActor. Add MeshStage.

--- laser/src/com/razh/laser/MeshActor.java
+++ laser/src/com/razh/laser/MeshActor.java
@@ -2,15 +2,31 @@ package com.razh.laser;
 
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class MeshActor extends Actor {
 	private Mesh mMesh;
 	private ShaderProgram mShaderProgram;
 
-	// Perimeter variables.
+	private Matrix4 mModelMatrix;
+
+	// Hull variables.
 	private float[] mVertices;
 	private short[] mIndices;
+
+	public void draw(ShaderProgram shaderProgram) {
+		if (mShaderProgram != shaderProgram) {
+			setShaderProgram(shaderProgram);
+		}
+
+		draw();
+	}
+
+	public void draw() {
+
+	}
 
 	public Mesh getMesh() {
 		return mMesh;
@@ -27,6 +43,18 @@ public class MeshActor extends Actor {
 	public void setShaderProgram(ShaderProgram shaderProgram) {
 		mShaderProgram = shaderProgram;
 	}
+
+	public Matrix4 getModelMatrix() {
+		return mModelMatrix;
+	}
+
+	public void setupModelMatrix() {
+		getModelMatrix().idt()
+		                .translate(getX(), getY(), 0.0f)
+		                .rotate(Vector3.Z, getRotation())
+		                .scale(getWidth(), getHeight(), 1.0f);
+	}
+
 
 	public float[] getVertices() {
 		return mVertices;
--- laser/src/com/razh/laser/Shader.java
+++ laser/src/com/razh/laser/Shader.java
@@ -1,5 +1,26 @@
 package com.razh.laser;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
 public class Shader {
+
+	public static ShaderProgram createSimpleShader() {
+		String vertexName = "shaders/simple.vert";
+		String fragmentName = "shaders/simple.frag";
+
+		return compileShader(Gdx.files.internal(vertexName).readString(),
+		                     Gdx.files.internal(fragmentName).readString());
+	}
+
+	public static ShaderProgram compileShader(String vertex, String fragment) {
+		ShaderProgram shader = new ShaderProgram(vertex, fragment);
+		if (!shader.isCompiled()) {
+			System.out.println(vertex);
+			System.out.println(fragment);
+		}
+
+		return shader;
+	}
 
 }
