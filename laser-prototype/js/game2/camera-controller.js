704067acae8c659553af14c9890dba9564467733
razh <somecrazyidiot@gmail.com>
2013-03-13 15:39:59 -0300

    Create camera-controller.js.

---
+++ laser-prototype/js/game2/camera-controller.js
@@ -0,0 +1,8 @@
+/**
+ * This thing controls the camera, updating it so that it's in
+ * the right position, with every visible entity fitting inside.
+ */
+
+var CameraController = function() {
+
+};
