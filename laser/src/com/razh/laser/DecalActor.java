366e9e425a4d9cf141d4595a6fb6d20aa83adc3f
razh <somecrazyidiot@gmail.com>
2013-03-18 14:32:50 -0300

    Update DecalActor.java with notes on drawing DecalBatches.

--- laser/src/com/razh/laser/DecalActor.java
+++ laser/src/com/razh/laser/DecalActor.java
@@ -1,5 +1,17 @@
 package com.razh.laser;
 
+/**
+ *  This class may not work as we cannot explictly pass uniforms to a given shader.
+ *  Need to write a custom DecalBatch which would allow us to do this.
+ * 
+ *  Perhaps we can use a custom DecalMaterial instead. With decalMaterial.set(), add decalMaterial.set(shader)
+ * 
+ *  How about customDecalBatch.render(ShaderProgram shader, Array<DecalActor> decalActors)?
+ *  This might work.
+ * 
+ *  Same as equivalent code in DecalBatch, but this time looping over the decalActors array, setting uniforms.
+ * 
+ */
 public class DecalActor extends Actor3D {
 	private Decal mDecal;
 
@@ -10,6 +22,13 @@ public class DecalActor extends Actor3D {
 		Texture texture = new Texture(pixmap);
 		TextureRegion region = new TextureRegion(texture);
 		setDecal(Decal.newDecal(region, true));
+	}
+
+	public void draw(DecalBatch decalBatch) {
+		draw(decalBatch, null);
+	}
+
+	public void draw(DecalBatch decalBatch, ShaderProgram shaderProgram) {
 	}
 
 	public Decal getDecal() {
